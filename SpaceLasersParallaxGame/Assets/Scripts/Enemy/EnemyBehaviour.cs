a40229c99932df30d5ae78ef215f28aef74a938a
dragomdv <dragdi15@gmail.com>
2017-11-13 23:44:50 -0500

    Added enemy projectiles

---
+++ SpaceLasersParallaxGame/Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+///  Class keeps track of enemy health and controlls its individual behaviour
+/// </summary>
+public class EnemyBehaviour : MonoBehaviour {
+
+	public float health = 150f;
+	public float laserSpeed = 10f;
+
+	private GameObject redLaser;
+
+	void Start(){
+		//TODO: This could be improved with GetComponentsInChildren<Projectile>();
+		redLaser = this.transform.parent.parent.GetChild (0).GetChild (5).GetChild (0).gameObject;
+	}
+
+	void Update(){
+		Vector3 startPosition = transform.position + new Vector3 (0f, -1f, 0f);
+		GameObject redLaserInstance = Instantiate (redLaser, startPosition, Quaternion.identity) as GameObject;
+		redLaserInstance.SetActive (true);
+		redLaserInstance.GetComponent<Rigidbody2D> ().velocity = new Vector3 (0f, -laserSpeed, 0f);
+	}
+
+	/// <summary>
+	///  If hit enemies health is decreased by projectile strength
+	/// </summary>
+	void OnTriggerEnter2D(Collider2D collider){
+		Projectile laser = collider.gameObject.GetComponent<Projectile> ();
+		if (laser != null) {
+			health -= laser.GetDamage ();
+			laser.Hit ();
+			if (health <= 0){
+				Destroy (gameObject);
+			}
+		}
+	}
+
+}
---
+++ SpaceLasersParallaxGame/Assets/Scripts/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  Class sets projectile damage and ships check whether they collide with projectile class
+/// </summary>
+public class Projectile : MonoBehaviour {
+
+	public float damage = 100f;
+
+	/// <summary>
+	///  Signals projectile hit object and should destroy itself
+	/// </summary>
+	public void Hit(){
+		Destroy (gameObject);
+	}
+
+	/// <summary>
+	///  Returns damage projectile causes
+	/// </summary>
+	public float GetDamage(){
+		return damage;
+	}
+
+}
